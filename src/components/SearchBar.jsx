d29c8fd4c9fb1912e47993bb8657c7316546b8d6
Gustavo Jardim <galvesjardim95@gmail.com>
2023-06-06 20:43:33 -0300

    feat(13): caso o retorno seja apenas 1 elemento, vai para a sua rota de detalhes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,30 +17,38 @@ function SearchBar() {
   const handleSearchBtn = async (id) => {
     switch (activeRadio) {
     case 'Ingredient':
-      try {
+      {
         const url = page === '/meals'
           ? await fetchMeals(id) : await fetchDrinks(id);
         console.log(url);
-      } catch (error) {
-        console.log(error);
       }
       break;
     case 'Name':
-      try {
-        const url = page === '/meals'
-          ? await fetchMealsName(id) : await fetchDrinksName(id);
-        console.log(url);
-      } catch (error) {
-        console.log(error);
+      {
+        const urlMeal = page === '/meals';
+        if (urlMeal) {
+          const meal = await fetchMealsName(id);
+          console.log(meal);
+          history.push(`/meals/${meal.meals[0].idMeal}`);
+        }
+        const urlDrink = page === '/drinks';
+        if (urlDrink) {
+          const drink = await fetchDrinksName(id);
+          console.log(drink);
+          history.push(`/drinks/${drink.drinks[0].idDrink}`);
+        }
       }
       break;
     case 'FirstLetter':
-      try {
+      if (search.length !== 1) {
+        global.alert(
+          'Your search must have only 1 (one) character',
+        );
+      }
+      {
         const url = page === '/meals'
           ? await fetchMealsFirstLetter(id) : await fetchDrinksFirstLetter(id);
         console.log(url);
-      } catch {
-        global.alert('Your search must have only 1 (one) character');
       }
       break;
     default:
@@ -61,7 +69,7 @@ function SearchBar() {
         <input
           type="radio"
           name="ingredient"
-          id="ingredient"
+          value="ingredient"
           data-testid="ingredient-search-radio"
           onClick={ () => setActieRadio('Ingredient') }
         />
@@ -71,7 +79,7 @@ function SearchBar() {
         <input
           type="radio"
           name="name"
-          id="name"
+          value="name"
           data-testid="name-search-radio"
           onClick={ () => setActieRadio('Name') }
         />
@@ -81,13 +89,17 @@ function SearchBar() {
         <input
           type="radio"
           name="first"
-          id="first-letter"
+          value="first-letter"
           data-testid="first-letter-search-radio"
           onClick={ () => setActieRadio('FirstLetter') }
         />
         First letter
       </label>
       <button
+        style={ {
+          backgroundColor: '#d3d3d3',
+          borderRadius: 50,
+        } }
         name="search-btn"
         data-testid="exec-search-btn"
         onClick={ () => handleSearchBtn(search) }
