0bd977fbeefdbae232e2868c512bf97c9d76e5ae
Gustavo Jardim <galvesjardim95@gmail.com>
2023-06-06 13:17:00 -0300

    feat(10): adicionado radio buttons

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,6 +10,36 @@ function SearchBar() {
         placeholder="Pesquisar"
         // onChange={ () => {} }
       />
+      <label htmlFor="radio">
+        <input
+          type="radio"
+          name="ingredient"
+          data-testid="ingredient-search-radio"
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name">
+        <input
+          type="radio"
+          name="name"
+          data-testid="name-search-radio"
+        />
+        Name
+      </label>
+      <label htmlFor="first">
+        <input
+          type="radio"
+          name="first"
+          data-testid="first-letter-search-radio"
+        />
+        First letter
+      </label>
+      <button
+        name="search-btn"
+        data-testid="exec-search-btn"
+      >
+        Buscar
+      </button>
     </div>
   );
 }
