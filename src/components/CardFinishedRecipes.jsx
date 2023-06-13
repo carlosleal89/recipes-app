e50991ae21095e4e16ba3d3eaa49ccf86ace9925
Matheus Ramos <theussramos@gmail.com>
2023-06-13 17:49:35 -0300

    feat(44-49): wip - card finished recipes

---
+++ src/components/CardFinishedRecipes.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
+import shareIcon from '../images/shareIcon.svg';
+import oneMeal from '../mocks/oneMeal';
+
+function CardFinishedRecipes() {
+  const [clipBoardmsg, setClipBoardMsg] = useState(false);
+
+  const tagName = 'Pasta';
+  const index = 0;
+  console.log(index);
+
+  const clipboardShare = (link) => {
+    copy(link);
+    setClipBoardMsg(true);
+  };
+
+  return (
+    <div>
+      <img
+        alt="recipe"
+        src={ oneMeal.meals[0].strMealThumb }
+        data-testid={ `${index}-horizontal-image` }
+      />
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        { oneMeal.meals[0].strCategory }
+      </p>
+      <p
+        data-testid={ `${index}-horizontal-name` }
+      >
+        { oneMeal.meals[0].strMeal }
+      </p>
+      <p
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        { (new Date()).toLocaleDateString() }
+      </p>
+      <p
+        data-testid={ `${index}-${tagName}-horizontal-tag` }
+      >
+        { oneMeal.meals[0].strTags }
+      </p>
+      <button
+        className="share-recipe-btn"
+        data-testid={ `${index}-horizontal-share-btn` }
+        onClick={ () => clipboardShare(window.location.href) }
+      >
+        <img
+          src={ shareIcon }
+          alt="share icon"
+        />
+      </button>
+      {
+        clipBoardmsg && <p className="clipboard-msg">Link copied!</p>
+      }
+    </div>
+  );
+}
+
+export default CardFinishedRecipes;
--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import TitleContext from '../context/TitleContext';
+import CardFinishedRecipes from '../components/CardFinishedRecipes';
 
 function DoneRecipes() {
   const { setTitle } = useContext(TitleContext);
@@ -12,6 +13,25 @@ function DoneRecipes() {
   return (
     <div>
       <Header />
+      <button
+        data-testid="filter-by-all-btn"
+        // onClick={  }
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        // onClick={  }
+      >
+        Food
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        // onClick={  }
+      >
+        Drink
+      </button>
+      <CardFinishedRecipes />
     </div>
   );
 }
