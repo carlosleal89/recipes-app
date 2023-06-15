226ae0e2f40a077ec7ea124e53e0850f94d528fd
Matheus Ramos <theussramos@gmail.com>
2023-06-15 20:05:21 -0300

    feat(49): encaminha para página de detalhes da receita a partir de receitas prontas

--- src/components/CardFinishedRecipes.jsx
+++ src/components/CardFinishedRecipes.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 
 function CardFinishedRecipes({ recipe, index }) {
-  console.log(recipe);
+  console.log(recipe.id);
   const [clipBoardmsg, setClipBoardMsg] = useState(false);
 
   const clipboardShare = (link) => {
@@ -18,22 +19,30 @@ function CardFinishedRecipes({ recipe, index }) {
 
   return (
     <div>
-      <img
-        alt="recipe"
-        src={ recipe.image }
-        data-testid={ `${index}-horizontal-image` }
-      />
+      <Link to={ `/${recipe.type}s/${recipe.id}` }>
+        <img
+          alt="recipe"
+          src={ recipe.image }
+          data-testid={ `${index}-horizontal-image` }
+          style={ {
+            width: 150,
+            height: 150,
+          } }
+        />
+      </Link>
       <p
         data-testid={ `${index}-horizontal-top-text` }
       >
         {recipe.category && recipe.nationality
           ? `${recipe.nationality} - ${recipe.category}` : recipe.alcoholicOrNot}
       </p>
-      <p
-        data-testid={ `${index}-horizontal-name` }
-      >
-        { recipe.name }
-      </p>
+      <Link to={ `/${recipe.type}s/${recipe.id}` }>
+        <p
+          data-testid={ `${index}-horizontal-name` }
+        >
+          { recipe.name }
+        </p>
+      </Link>
       <p
         data-testid={ `${index}-horizontal-done-date` }
       >
